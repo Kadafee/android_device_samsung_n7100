d00a6317972ac4977c997a33aff14cdb02eadca4
marc0601 <marckhalil06@gmail.com>
2018-11-27 20:04:43 +0200

    n7100: add USB Mass Storage support [3/3]

--- n7100.mk
+++ n7100.mk
@@ -30,7 +30,8 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
-    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc
+    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
+    $(LOCAL_PATH)/rootdir/init.target.usb.rc:root/init.target.usb.rc
 
 # HIDL
 PRODUCT_COPY_FILES += \
@@ -136,5 +137,9 @@ PRODUCT_COPY_FILES += \
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
+
+# UMS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ums_init.sh:system/bin/ums_init.sh
 
 $(call inherit-product-if-exists, vendor/samsung/n7100/n7100-vendor.mk)
