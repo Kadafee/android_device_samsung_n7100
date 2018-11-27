html6405/android_device_samsung_i9300

i9300: move RIL defines/header to common [1/2]
Change-Id: Ie3c244f1a8583d95c66a4b685711f0199ec3428f

BoardConfig.mk
@@ -23,13 +23,6 @@ MALLOC_SVELTE := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 LIBART_IMG_BASE := 0x30000000
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-BOARD_MODEM_TYPE := xmm6262
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/i9300/include
-BOARD_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-BOARD_RIL_CLASS := ../../../device/samsung/i9300/ril
-
 # Graphics
 TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 
@@ -39,6 +32,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := lineageos_i9300_defconfig
+
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/i9300/include
 
 # Cache
 BOARD_CACHEIMAGE_PARTITION_SIZE := 1048576

Kadafee/android_device_samsung_n7100

BoardConfig.mk
@@ -26,13 +26,6 @@ WIFI_DRIVER_MODULE_PATH :=
 MALLOC_SVELTE := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-BOARD_MODEM_TYPE := xmm6262
-BOARD_RIL_CLASS := ../../../device/samsung/n7100/ril
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/n7100/include
-BOARD_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-
 # Graphics
 TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 
@@ -58,6 +51,8 @@ TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
 # Camera Hacks
 BOARD_GLOBAL_CFLAGS += -DMETADATA_CAMERA_SOURCE
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/n7100/include
 
 # Cache
 BOARD_CACHEIMAGE_PARTITION_SIZE := 1048576